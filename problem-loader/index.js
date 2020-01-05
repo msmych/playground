c30d49badc0652045ad30b3ecf5e2371e29c47f3
matvey <msmych@yandex.com>
2020-01-05 12:11:34 +0300

    Saving problems to current directory

--- problem-loader/index.js
+++ problem-loader/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const {Builder, By, until, promise} = require('selenium-webdriver');
 const {createProblem} = require('./problem');
-const fs = require('fs');
+const {existsSync, mkdirSync, writeFileSync} = require('fs');
 
 const titleLocator = By.css("div[data-cy='question-title']");
 const diffLocator = By.css("div[diff]");
@@ -17,26 +17,22 @@ const textLocator = By.css(".question-content__JfgR > div");
     console.log(`Waiting for page to load`);
     await driver.wait(until.elementLocated(titleLocator));
     const problemData = { url: url };
-    console.log('Getting problem title');
     await driver.findElement(titleLocator).getText()
         .then(title => problemData.title = title);
-    console.log('Getting problem difficulty');
     await driver.findElement(diffLocator).getAttribute('diff')
         .then(diff => problemData.diff = diff);
-    console.log('Getting problem tags');
     await promise.map(driver.findElements(tagLocator), tag => tag.getText())
         .then(tags => problemData.tags = tags);
-    console.log('Getting problem text');
     await driver.findElement(textLocator).getAttribute('innerHTML')
         .then(text => problemData.text = text);
     const problem = createProblem(problemData, 'html');
     console.log(problem);
-    if (!fs.existsSync(`./../${problem.title}`)) {
+    if (!existsSync(`./${problem.title}`)) {
         console.log(`Creating directory ${problem.title}`);
-        fs.mkdirSync(`./../${problem.title}`);
+        mkdirSync(`./${problem.title}`);
     }
     console.log(`Writing to file ${problem.fileName}`);
-    fs.writeFileSync(`./../${problem.title}/${problem.fileName}`, problem.format());
+    writeFileSync(`./${problem.title}/${problem.fileName}`, problem.format());
     console.log('Done');
 })()
 
