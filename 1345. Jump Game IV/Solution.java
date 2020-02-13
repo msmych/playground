ecbb5d28e681a8f58cba026b4b4268595080bb13
Matvey <matvey.smychkov@revolut.com>
2020-02-13 16:27:56 +0300

    1345. Jump Game IV

--- 1345. Jump Game IV/Solution.java
+++ 1345. Jump Game IV/Solution.java
@@ -1,6 +1,52 @@
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.IntStream.range;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
 class Solution {
     public int minJumps(int[] arr) {
-        return 0;
+        if (arr.length == 1) {
+            return 0;
+        }
+        if (arr[0] == arr[arr.length - 1]) {
+            return 1;
+        }
+        if (arr[0] == arr[arr.length - 2]) {
+            return 2;
+        }
+        Map<Integer, List<Integer>> map = range(0, arr.length).boxed().collect(groupingBy(i -> arr[i]));
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        Set<Integer> visited = new HashSet<>();
+        int jumps = 0;
+        while (!queue.isEmpty()) {
+            for (int q = queue.size(); q > 0; q--) {
+                Integer i = queue.poll();
+                if (i == arr.length - 1) {
+                    return jumps;
+                }
+                visited.add(i);
+                if (i > 0 && !visited.contains(i - 1)) {
+                    queue.offer(i - 1);
+                }
+                if (i < arr.length - 1 && !visited.contains(i + 1)) {
+                    queue.offer(i + 1);
+                }
+                int n = arr[i];
+                for (Integer next : map.get(n)) {
+                    if (!visited.contains(next)) {
+                        queue.offer(next);
+                    }
+                }
+            }
+            jumps++; 
+        }
+        return jumps;
     }
 
     // java Solution.java "[100,-23,-23,404,100,23,23,23,3,404]" "3" "[7]" "0" "[7,6,9,6,9,6,9,7]" "1" "[6,1,9]" "2" "[11,22,7,7,7,7,7,7,7,22,13]" "3"
