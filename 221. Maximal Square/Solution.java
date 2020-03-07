742142d7662c8fa49076817e15000bdcb3bb8bac
Matvey <realsmych@gmail.com>
2020-03-07 16:02:16 +0300

    221. Maximal Square

--- 221. Maximal Square/Solution.java
+++ 221. Maximal Square/Solution.java
@@ -1,10 +1,33 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        return 0;
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[matrix[0].length + 1];
+        int max = 0;
+        int last = 0;
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                int val = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = IntStream.of(dp[j - 1], last, dp[j]).min().getAsInt() + 1;
+                    if (dp[j] > max) {
+                        max = dp[j];
+                    }
+                } else {
+                    dp[j] = 0;
+                }
+                last = val;
+            }
+        }
+        return max * max;
     }
 
-    // java Solution.java "[['1','0','1','0','0',],['1','0','1','1','1'],['1','1','1','1','1'],['1','0','0','1','0']]" "4"
+    // java Solution.java "[[1,0,1,0,0],[1,0,1,1,1],[1,1,1,1,1],[1,0,0,1,0]]" 4
     public static void main(String... args) {
+        new Solution().maximalSquare(array("[[1,0,1,0,0],[1,0,1,1,1],[1,1,1,1,1],[1,0,0,1,0]]"));
         for (int i = 0; i < args.length; i += 2) {
             String matrix = args[i], expected = args[i + 1];
             System.out.println(String.format(
