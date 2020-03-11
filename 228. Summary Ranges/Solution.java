06568757260e32e837db106cf08379427ee8447a
Matvey <matvey.smychkov@revolut.com>
2020-03-11 12:26:21 +0300

    228. Summary Ranges

--- 228. Summary Ranges/Solution.java
+++ 228. Summary Ranges/Solution.java
@@ -1,8 +1,34 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.OptionalInt;
+
+import static java.util.OptionalInt.empty;
 
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        return null;
+        List<String> ranges = new ArrayList<>();
+        OptionalInt last = empty();
+        for (int num : nums) {
+            if (last.isEmpty()) {
+                last = OptionalInt.of(num);
+                ranges.add(String.valueOf(num));
+            } else {
+                Integer n = last.getAsInt();
+                if (num == n + 1) {
+                    last = OptionalInt.of(num);
+                } else {
+                    if (n > Integer.parseInt(ranges.get(ranges.size() - 1))) {
+                        ranges.set(ranges.size() - 1, ranges.get(ranges.size() - 1) + "->" + n);
+                    }
+                    last = OptionalInt.of(num);
+                    ranges.add(String.valueOf(num));
+                }
+            }
+        }
+        if (last.isPresent() && last.getAsInt() > Integer.parseInt(ranges.get(ranges.size() - 1))) {
+            ranges.set(ranges.size() - 1, ranges.get(ranges.size() - 1) + "->" + last.getAsInt());
+        }
+        return ranges;
     }
 
     // java Solution.java "[0,1,2,4,5,7]" "[0->2,4->5,7]" "[0,2,3,4,6,8,9]" "[0,2->4,6,8->9]"
