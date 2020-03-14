ffb6f509a725ce0a91a6824b3538904b8e32082c
Matvey <realsmych@gmail.com>
2020-03-14 12:50:18 +0300

    260. Single Number III

---
+++ 260. Single Number III/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        return new int[0];
+    }
+
+    // java Solution.java "[1,2,1,3,2,5]" "[3,5]"
+    public static void main(String... args) {
+        for (int i = 0; i < args.length; i += 2) {
+            String nums = args[i], expected = args[i + 1];
+            System.out.println(String.format(
+                "Output: %s | Expected: %s | Input: nums = %s",
+                string(new Solution().singleNumber(array(nums))), expected, nums));
+        }
+    }
+
+    private static int[] array(String s) {
+        String[] elements = s.substring(1, s.length() - 1).replaceAll(" ", "").split(",");
+        int[] arr = new int[elements.length];
+        for (int i = 0; i < elements.length; i++)
+            arr[i] = Integer.parseInt(elements[i]);
+        return arr;
+    }
+
+    private static String string(int[] arr) {
+        String s = "";
+        for (int n : arr) s += "," + n;
+        if (!s.isEmpty()) s = s.substring(1);
+        return "[" + s + "]";
+    }
+}
