7af6d4884255fba39c36288e1bc9fd75a0d3fd68
Matvey <matvey.smychkov@revolut.com>
2020-05-02 10:59:12 +0300

    335. Self Crossing

--- 335. Self Crossing/Solution.java
+++ 335. Self Crossing/Solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isSelfCrossing(int[] x) {
+        if (x.length < 4) {
+            return false;
+        }
+        for (var i = 3; i < x.length; i++) {
+            if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) {
+                return true;
+            }
+            if (i >= 4 && x[i - 1] == x[i - 3] && x[i - 4] + x[i] >= x[i - 2]) {
+                return true;
+            }
+            if (i >= 5 && x[i - 4] + x[i] >= x[i - 2] && x[i - 1] >= x[i - 3] - x[i - 5] && x[i - 2] >= x[i - 4] && x[i - 1] <= x[i - 3]) {
+                return true;
+            }
+        }
         return false;
     }
 
