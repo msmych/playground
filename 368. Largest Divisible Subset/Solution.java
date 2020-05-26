304164425ad773607177facc79c70785e83cdc5e
Matvey <realsmych@gmail.com>
2020-05-26 21:54:16 +0300

    368. Largest Divisible Subset

--- 368. Largest Divisible Subset/Solution.java
+++ 368. Largest Divisible Subset/Solution.java
@@ -1,11 +1,43 @@
 import java.util.*;
 
+import static java.util.Arrays.*;
+
+
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        return null;
+        if (nums.length == 0) {
+            return List.of();
+        }
+        sort(nums);
+        var arr = new int[nums.length];
+        var parent = new int[nums.length];
+        fill(parent, -1);
+        arr[0] = 1;
+        var max = 0;
+        var maxIndex = 0;
+        for (var i = 1; i < nums.length; i++) {
+            arr[i] = 1;
+            for (var j = i - 1; j >= 0; j--) {
+                if (nums[i] % nums[j] == 0) {
+                    if (arr[j] + 1 > arr[i]) {
+                        arr[i] = arr[j] + 1;
+                        parent[i] = j;
+                    }
+                }
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        var subset = new ArrayList<Integer>();
+        for (var i = maxIndex; i >= 0; i = parent[i]) {
+            subset.add(0, nums[i]);
+        }
+        return subset;
     }
 
-    // java Solution.java "[1,2,3]" "[1,2]" "[1,2,4,8]" "[1,2,4,8]"
+    // java Solution.java "[1,2,3]" "[1,2]" "[1,2,4,8]" "[1,2,4,8]" "[2,3,8,9,27]" "[3,9,27]"
     public static void main(String... args) {
         for (int i = 0; i < args.length; i += 2) {
             String nums = args[i], expected = args[i + 1];
