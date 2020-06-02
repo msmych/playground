dd95717ae4fac92f8768485d4b4fba8fe6cc1805
Matvey <matvey.smychkov@revolut.com>
2020-06-02 23:04:51 +0300

    1466. Reorder Routes to Make All Paths Lead to the City Zero

--- 1466. Reorder Routes to Make All Paths Lead to the City Zero/Solution.java
+++ 1466. Reorder Routes to Make All Paths Lead to the City Zero/Solution.java
@@ -1,6 +1,40 @@
+import java.util.*;
+
 class Solution {
+
+    private final Set<Integer> visited = new HashSet<>();
+    private final Map<Integer, Set<Integer>> original = new HashMap<>();
+    private final Map<Integer, Set<Integer>> reversed = new HashMap<>();
+
     public int minReorder(int n, int[][] connections) {
-        return 0;
+        for (var i = 0; i < n; i++) {
+            original.putIfAbsent(i, new HashSet<>());
+            reversed.putIfAbsent(i, new HashSet<>());
+        }
+        for (var connection : connections) {
+            original.get(connection[1]).add(connection[0]);
+            reversed.get(connection[0]).add(connection[1]);
+        }
+        return nextReorder(0);
+    }
+
+    private int nextReorder(int i) {
+        if (visited.contains(i)) {
+            return 0;
+        }
+        visited.add(i);
+        var reorder = 0;
+        for (var next : original.get(i)) {
+            reorder += nextReorder(next);
+        }
+        for (var next : reversed.get(i)) {
+            if (visited.contains(next)) {
+                continue;
+            }
+            reorder++;
+            reorder += nextReorder(next);
+        }
+        return reorder;
     }
 
     // java Solution.java "6" "[[0,1],[1,3],[2,3],[4,0],[4,5]]" "3" "5" "[[1,0],[1,2],[3,2],[3,4]]" "2" "3" "[[1,0],[2,0]]" "0"
