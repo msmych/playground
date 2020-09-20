430ab5419bb36c3e02b1057567bfad770f0bb8bc
Matvey <realsmych@gmail.com>
2020-09-20 16:48:23 +0300

    980. Unique Paths III

--- 980. Unique Paths III/Solution.java
+++ 980. Unique Paths III/Solution.java
@@ -1,6 +1,40 @@
+import java.util.*;
+
+import static java.util.Arrays.*;
+
 class Solution {
+
+    private int[][] grid;
+    private long cells;
+
     public int uniquePathsIII(int[][] grid) {
-        return 0;
+        this.grid = grid;
+        cells = stream(grid).mapToLong(row -> stream(row).filter(c -> c == 0).count()).sum();
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    return nextPaths(i - 1, j, new ArrayList<>()) +
+                        nextPaths(i, j + 1, new ArrayList<>()) +
+                        nextPaths(i + 1, j, new ArrayList<>()) +
+                        nextPaths(i, j - 1, new ArrayList<>());
+                }
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private int nextPaths(int i, int j, List<Integer> visited) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == -1 || grid[i][j] == 1 || visited.contains(1000 * i + j)) {
+            return 0;
+        }
+        if (grid[i][j] == 2) {
+            return visited.size() == cells ? 1 : 0;
+        }
+        visited.add(1000 * i + j);
+        return nextPaths(i - 1, j, new ArrayList<>(visited)) +
+            nextPaths(i, j + 1, new ArrayList<>(visited)) +
+            nextPaths(i + 1, j, new ArrayList<>(visited)) +
+            nextPaths(i, j - 1, new ArrayList<>(visited));
     }
 
     // java Solution.java "[[1,0,0,0],[0,0,0,0],[0,0,2,-1]]" "2" "[[1,0,0,0],[0,0,0,0],[0,0,0,2]]" "4" "[[0,1],[2,0]]" "0"
