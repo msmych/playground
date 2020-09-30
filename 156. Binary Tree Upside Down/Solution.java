4db29cd732da281111cf773796e1405aafcab266
Matvey <matvey.smychkov@revolut.com>
2020-09-30 22:34:39 +0300

    156. Binary Tree Upside Down

---
+++ 156. Binary Tree Upside Down/Solution.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Solution {
+
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        return upsideNext(root, null, null);
+    }
+
+    private TreeNode upsideNext(TreeNode node, TreeNode parent, TreeNode right) {
+        if (node == null) {
+            return parent;
+        }
+        TreeNode leftNode = node.left, rightNode = node.right;
+        node.left = right;
+        node.right = parent;
+        return upsideNext(leftNode, node, rightNode);
+    }
+
+    // java Solution.java "[1,2,3,4,5]" "[4,5,2,null,null,3,1]" "[]" "[]" "[1]" "[1]"
+    public static void main(String... args) {
+        for (int i = 0; i < args.length; i += 2) {
+            String root = args[i], expected = args[i + 1];
+            System.out.println(String.format(
+                "Output: %s | Expected: %s | Input: root = %s",
+                string(new Solution().upsideDownBinaryTree(treeNode(root))), expected, root));
+        }
+    }
+
+    private static TreeNode treeNode(String s) {
+        s = s.replace("[", "").replace("]", "").replaceAll(" ", "");
+        if (s.isEmpty()) return null;
+        String[] elements = s.split(",");
+        TreeNode[] nodes  = new TreeNode[elements.length];
+        Stack<TreeNode> stack = new Stack<>();
+        for (int i = elements.length - 1, n = 0; i >= 0; i--, n++) {
+            TreeNode node = (elements[i].equals("null")) ? null : new TreeNode(Integer.parseInt(elements[i]));
+            nodes[elements.length - n - 1] = node;
+            stack.push(node);
+        }
+        TreeNode root = stack.pop();
+        for (TreeNode node : nodes) {
+            if (node != null) {
+                if (!stack.empty()) node.left = stack.pop();
+                if (!stack.empty()) node.right = stack.pop();
+            }
+        }
+        return root;
+    }
+
+    private static String string(TreeNode root) {
+        if (root == null) return "[]";
+        String s = "";
+        TreeNode[] nodes = new TreeNode[]{root};
+        for (boolean hasNodes = true; hasNodes;) {
+            hasNodes = false;
+            String level = "";
+            TreeNode[] next = new TreeNode[2 * nodes.length];
+            for (int i = 0; i < nodes.length; i++) {
+                level += nodes[i] == null ? "null," : nodes[i].val + ",";
+                if (nodes[i] != null) {
+                    hasNodes = true;
+                    next[2 * i] = nodes[i].left;
+                    next[2 * i + 1] = nodes[i].right;
+                }
+            }
+            while (level.endsWith("null,null,")) level = level.substring(0, level.length() - 5);
+            s += level;
+            nodes = next;
+        }
+        while (s.endsWith("null,")) s = s.substring(0, s.length() - 5);
+        return "[" + s.substring(0, s.length() - 1) + "]";
+    }
+}
+
+// ~~~ Please don't copy to LeetCode starting from this line
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
