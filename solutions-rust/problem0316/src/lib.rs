cff22f6aa0c964daf256c01d105f6ad1e7d180e5
Matvey <matvey@sayhello.cash>
2023-09-27 19:57:35 +0100

    0316 rust problem

---
+++ solutions-rust/problem0316/src/lib.rs
@@ -0,0 +1,28 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicate_letters(s: String) -> String {
+        String::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let result = Solution::remove_duplicate_letters("bcabc".into());
+
+        assert_eq!("abc".to_string(), result);
+    }
+
+    #[test]
+    fn case2() {
+        let result = Solution::remove_duplicate_letters("bcabccbacdcbc".into());
+
+        assert_eq!("acdb".to_string(), result);
+    }
+}
