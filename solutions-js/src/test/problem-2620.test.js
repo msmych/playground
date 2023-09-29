bd2af408743c4d74b245feb79c5924ebc776eaec
Matvey <matvey@sayhello.cash>
2023-09-29 18:27:43 +0100

    2620 js solution

---
+++ solutions-js/src/test/problem-2620.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "bun:test";
+import createCounter from "../problem-2620";
+
+describe('2620', () => {
+    test('case1', () => {
+        const counter = createCounter(10)
+
+        expect(counter()).toBe(10)
+        expect(counter()).toBe(11)
+        expect(counter()).toBe(12)
+    })
+
+    test('case2', () => {
+        const counter = createCounter(-2)
+
+        expect(counter()).toBe(-2)
+        expect(counter()).toBe(-1)
+        expect(counter()).toBe(0)
+        expect(counter()).toBe(1)
+        expect(counter()).toBe(2)
+    })
+})
