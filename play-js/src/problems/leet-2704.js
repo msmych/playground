3efa9a404d5f3fdda640eeeaf4babb703071d9b4
Matvey <matvey@sayhello.cash>
2023-10-02 16:57:23 +0100

    2704 js solution

---
+++ play-js/src/problems/leet-2704.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(v) {
+            if (val !== v) {
+                throw new Error("Not Equal")
+            }
+            return true
+        },
+        notToBe: function(v) {
+            if (val === v) {
+                throw new Error("Equal")
+            }
+            return true
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+export default expect;
---
+++ play-js/src/test/problems/leet-2704.test.js
@@ -0,0 +1,12 @@
+import { describe, test } from "bun:test";
+import expect from "../../problems/leet-2704";
+
+describe('2704', () => {
+    test('case1', () => {
+        expect(5).toBe(5)
+    })
+
+    test('case2', () => {
+        expect(5).notToBe(null)
+    })
+})
