fc1e3cbc5973bfdc8aac3bd38a833a1fa178b828
Matvey <matvey@sayhello.cash>
2023-10-02 16:31:37 +0100

    2667 js solution

---
+++ play-js/src/test/problems/leet-2667.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "bun:test";
+import createHelloWorld from "../../problems/leet-2667" 
+
+describe('2667', () => {
+    test('case1', () => {
+        expect(createHelloWorld([])()).toBe("Hello World")
+    })
+
+    test('case2', () => {
+        expect(createHelloWorld([{},null,42])()).toBe("Hello World")
+    })
+})
