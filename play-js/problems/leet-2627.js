89c1f7128de9815b61db64f2ad0d1fba3122aa7d
Matvey <realsmych@gmail.com>
2023-10-13 23:44:15 +0100

    2627 js solution

---
+++ play-js/problems/leet-2627.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+    let timerId
+    return function (...args) {
+        if (timerId) {
+            clearTimeout(timerId)
+        }
+        timerId = setTimeout(() => fn(...args), t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
+
+export default debounce;
