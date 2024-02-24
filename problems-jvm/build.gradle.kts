c43a0a4b64a5a3e77478dc2ec6d5595463b604fe
Matvey <realsmych@gmail.com>
2024-02-24 21:48:17 +0000

    problems-jvm: 2092 kotlin solution

---
+++ problems-jvm/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+    kotlin("jvm") version "1.9.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
